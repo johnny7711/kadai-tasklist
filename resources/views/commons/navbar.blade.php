3884775f19cc3c88b2269fd0f954a4b7fa7a2dea
Johnny <masa.mwam11@gmail.com>
2017-05-11 03:58:22 +0000

    fix nav

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -13,16 +13,17 @@
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     @if (Auth::check())
+                    <li>{!! link_to_route('tasks.create', '新規タスクの投稿') !!}</li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
                          <ul class="dropdown-menu">
                                 <li>{!! link_to_route('logout.get', 'Logout') !!}</li>
-                            </ul>
+                        </ul>
                     </li>
                     @else
                         <li>{!! link_to_route('signup.get', 'Signup') !!}</li>
                         <li>{!! link_to_route('login.get', 'Login') !!}</li>
-                        <!--<li>{!! link_to_route('tasks.create', '新規タスクの投稿') !!}</li>-->
+                       
                     @endif
                 </ul>
             </div>
